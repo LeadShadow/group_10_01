e7d66cb195fb573dde649e00974a3b90bf291140
OlexandrSamus <olexandr.samus.2004@gmail.com>
2023-02-18 18:12:35 +0300

    add lesson15

--- hw13/task1.py
+++ hw13/task1.py
@@ -26,8 +26,21 @@
 # Например: вызов get_random_winners(2, participants) случайно может вернуть список как:
 # ['60577ce4b536f8259cc225d2', '605b89080c318d66862db390'].
 
+participants = {
+    "603d2cec9993c627f0982404": "test@test.com",
+    "603f79022922882d30dd7bb6": "test11@test.com",
+    "60577ce4b536f8259cc225d2": "test2@test.com",
+    "605884760742316c07eae603": "vitanlhouse@gmail.com",
+     "605b89080c318d66862db390": "elhe2013@gmail.com",
+ }
+
 import random
 
-
 def get_random_winners(quantity, participants):
-    pass
+    participants_keys = list(participants.keys())
+    if quantity > len(participants):
+        return []
+    random.shuffle(participants_keys)
+    return participants_keys[:quantity]
+
+print(get_random_winners(3, participants))
