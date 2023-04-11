5ba2e14ffe86c828fc321fa3b941073f3485e5a2
olex <olexandr.samus.2004@gmail.com>
2023-04-11 17:55:27 +0300

    add lesson22

--- hw19/task1.py
+++ hw19/task1.py
@@ -11,3 +11,33 @@
 # Создайте функцию get_favorites(contacts), которая будет возвращать список, содержащий только
 # избранные контакты. Используйте при этом функцию filter, чтобы отфильтровать по полю favorite
 # только избранные контакты.
+
+contacts = [
+    {
+        "name": "Allen Raymond",
+        "email": "nulla.ante@vestibul.co.uk",
+        "phone": "(992) 914-3792",
+        "favorite": False,
+    },
+    {
+        "name": "John Smith",
+        "email": "john.smith@example.com",
+        "phone": "(555) 123-4567",
+        "favorite": True,
+    },
+    {
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com",
+        "phone": "(555) 987-6543",
+        "favorite": True,
+    },
+
+]
+
+
+
+def get_favorites(contacts):
+    return list(filter(lambda contact: contact["favorite"], contacts))
+
+favorites = get_favorites(contacts)
+print(favorites)
---
+++ lesson22/assistant.py
@@ -0,0 +1,22 @@
+from handlers import *
+
+COMMANDS = {'hello': hello, 'add': add_contact, 'change': change_contact, 'show_phone': show_phone,
+            'show all': show_all, 'exit': goodbye, 'close': goodbye, '.': goodbye, 'stop': goodbye}
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
